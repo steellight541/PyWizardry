d6dadb6dedc7f82f10de1ac76df9ea941288ff4e
Nigel Belderbos <nigel.belderbos@outlook.com>
2024-05-14 09:34:23 +0200

    changed main.py to main.pyw

--- menu/preferences.py
+++ menu/preferences.py
@@ -1,4 +1,5 @@
-from tkinter import Menu 
+from tkinter import Menu
+
 
 class MyPreferencesMenu(Menu):
     def __init__(self, master):
@@ -17,7 +18,7 @@ class MyZenModeMenu(Menu):
 
     def toggle_zen_mode(self):
         self.iszen = not self.iszen
-        self.master.attributes("-fullscreen", self.iszen) # type: ignore
+        self.master.attributes("-fullscreen", self.iszen)  # type: ignore
 
 
 class MyStylesMenu(Menu):
@@ -26,7 +27,6 @@ class MyStylesMenu(Menu):
 
         self.master = master
         self.add_cascade(label="Themes", menu=MyThemesMenu(master))
-
 
 
 class MyThemesMenu(Menu):
@@ -38,10 +38,10 @@ class MyThemesMenu(Menu):
         self.add_command(label="Dark Theme", command=self.set_dark_theme)
 
     def set_line_numbers(self, fg, bg):
-        self.master.text_block.lines_counter.config(fg=fg, bg=bg) # type: ignore
+        self.master.text_block.lines_counter.config(fg=fg, bg=bg)  # type: ignore
 
     def set_text_block(self, fg, bg):
-        self.master.text_block.config(fg=fg, bg=bg) # type: ignore
+        self.master.text_block.config(fg=fg, bg=bg)  # type: ignore
 
     def set_cursor_color(self, color):
         self.master.text_block.config(insertbackground=color)
@@ -49,10 +49,9 @@ class MyThemesMenu(Menu):
     def set_light_theme(self):
         self.set_cursor_color("black")
         self.set_line_numbers("black", "lightgrey")
-        self.set_text_block("black", "white")
+        self.set_text_block("black", "lightgrey")
 
     def set_dark_theme(self):
-        self.set_cursor_color("white")
-        self.set_line_numbers("white", "black")
+        self.set_cursor_color("lightgrey")
+        self.set_line_numbers("lightgrey", "black")
         self.set_text_block("white", "black")
-
